008642a42404849f9ee284eca12890d7402aec5c
Masud Rana <masud.aiub.cse@gmail.com>
2025-08-13 10:32:40 +0200

    Event delete feature added

--- booking-service/src/main/java/com/pm/bookingservice/controller/TicketController.java
+++ booking-service/src/main/java/com/pm/bookingservice/controller/TicketController.java
@@ -2,11 +2,17 @@ package com.pm.bookingservice.controller;
 
 import com.pm.bookingservice.model.Ticket;
 import com.pm.bookingservice.service.TicketService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/ticket")
@@ -21,5 +27,15 @@ public class TicketController {
     @GetMapping
     public List<Ticket> getEvents() {
         return ticketService.getTickets();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteTicket(@PathVariable UUID id){
+
+        ticketService.deleteTicket(id);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(Map.of("message","Event deleted"));
     }
 }
--- booking-service/src/main/java/com/pm/bookingservice/service/TicketService.java
+++ booking-service/src/main/java/com/pm/bookingservice/service/TicketService.java
@@ -4,7 +4,9 @@ import com.pm.bookingservice.enums.TicketStatus;
 import com.pm.bookingservice.model.Ticket;
 import com.pm.bookingservice.repository.TicketRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,5 +37,15 @@ public class TicketService {
             tickets.add(new Ticket(eventId, TicketStatus.AVAILABLE));
         }
         ticketRepository.saveAll(tickets);
+    }
+
+    public void deleteTicket(UUID id) {
+
+        boolean existingTicket = ticketRepository.existsByEventId(id);
+
+        if(!existingTicket)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found");
+
+        ticketRepository.deleteById(id);
     }
 }
