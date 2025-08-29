f59426d7ee60acdd484e80a4abe37e02601de087
Masud Rana <masud.aiub.cse@gmail.com>
2025-08-29 15:34:30 +0200

    Remove unnecessary dependencies

--- booking-service/src/main/java/com/pm/bookingservice/service/TicketService.java
+++ booking-service/src/main/java/com/pm/bookingservice/service/TicketService.java
@@ -10,15 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
